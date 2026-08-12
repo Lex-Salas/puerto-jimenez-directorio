630a6b9457d28a10dfd00d1093854d921d15251c
Alexander Salas <asalasgonza@icloud.com>
2026-08-12 01:00:23 -0600

    Incluye la última ampliación en caché PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pj-directorio-v3';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./extra-businesses-1.js','./extra-businesses-2.js','./extra-businesses-3.js','./extra-businesses-4.js','./extra-businesses-5.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='pj-directorio-v4';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./extra-businesses-1.js','./extra-businesses-2.js','./extra-businesses-3.js','./extra-businesses-4.js','./extra-businesses-5.js','./extra-businesses-6.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
